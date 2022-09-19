e66700927838836338df03562e13f73ff877b6d5
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2022-09-19 21:33:54 +0200

    Set max-age=86400 on the JSON gallery file

--- exampleSite/deploy.sh
+++ exampleSite/deploy.sh
@@ -1,3 +1,3 @@
 rm -rf public
 hugo --minify
-s3deploy -source=public/ -region=eu-north-1 -bucket=staticbattery.com -acl='public-read' -distribution-id=E15NLNM65B1T1F
+s3deploy -source=public/ -region=eu-north-1 -bucket=staticbattery.com -acl='public-read' -distribution-id=E15NLNM65B1T1F 
